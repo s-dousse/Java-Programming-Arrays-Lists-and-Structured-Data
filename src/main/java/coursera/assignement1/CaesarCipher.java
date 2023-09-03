47df9f7d7564fb984e6a98b59772403e77ae7187
Sarah <dousse.sarah@gmail.com>
2023-09-03 18:12:25 +0100

    Caesar Cipher: refactoring, add helper method setNewCharacter()

--- src/main/java/coursera/assignement1/CaesarCipher.java
+++ src/main/java/coursera/assignement1/CaesarCipher.java
@@ -2,65 +2,48 @@ package coursera.assignement1;
 
 public class CaesarCipher {
 
-    private String alphabet;
+    private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";;
     private String shiftedAlphabetOne;
     private String  shiftedAlphabetTwo;
 
     public CaesarCipher(int key) {
-        key = validate(key);
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        shiftedAlphabetOne = alphabet.substring(key) +
-                alphabet.substring(0, key);
+        shiftedAlphabetOne = shiftBy(validate(key));
     }
 
     public CaesarCipher(int keyOne, int keyTwo) {
-        keyOne = validate(keyOne);
-        keyTwo = validate(keyTwo);
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        shiftedAlphabetOne = alphabet.substring(keyOne) +
-                alphabet.substring(0, keyOne);
-        shiftedAlphabetTwo = alphabet.substring(keyTwo) +
-                alphabet.substring(0, keyTwo);
+        shiftedAlphabetOne = shiftBy(validate(keyOne));
+        shiftedAlphabetTwo = shiftBy(validate(keyTwo));
     }
 
     public String encryptWithOneKey(String input) {
-        StringBuilder encrypted = new StringBuilder(input);
-        for(int i = 0; i < encrypted.length(); i++) {
-            char currChar = encrypted.charAt(i);
-            int idx = alphabet.indexOf(Character.toUpperCase(currChar));
+        StringBuilder encryptedMsg = new StringBuilder(input);
+        for(int i = 0; i < encryptedMsg.length(); i++) {
+            char currChar = encryptedMsg.charAt(i);
+            int idx = ALPHABET.indexOf(Character.toUpperCase(currChar));
             if (idx != -1){
                 char newChar = shiftedAlphabetOne.charAt(idx);
-                if (Character.isUpperCase(currChar)) {
-                    encrypted.setCharAt(i, Character.toUpperCase(newChar));
-                } else {
-                    encrypted.setCharAt(i, Character.toLowerCase(newChar));
-                }
+                setNewCharacter(currChar, newChar, encryptedMsg, i );
             }
         }
-        return encrypted.toString();
+        return encryptedMsg.toString();
     }
 
     public String encryptWithTwoKeys(String input) {
-        StringBuilder encrypted = new StringBuilder(input);
-        for (int i = 0; i < encrypted.length(); i++) {
-            char currChar = encrypted.charAt(i);
-            int idx = alphabet.indexOf(Character.toUpperCase(currChar));
-            char newChar;
+        StringBuilder encryptedMsg = new StringBuilder(input);
+        for (int i = 0; i < encryptedMsg.length(); i++) {
+            char currChar = encryptedMsg.charAt(i);
+            int idx = ALPHABET.indexOf(Character.toUpperCase(currChar));
             if (idx != -1) {
-                if (i % 2 == 0) {
-                    newChar = shiftedAlphabetOne.charAt(idx);
-                } else {
-                    newChar = shiftedAlphabetTwo.charAt(idx);
-                }
-
-                if (Character.isUpperCase(currChar)) {
-                    encrypted.setCharAt(i, Character.toUpperCase(newChar));
-                } else {
-                    encrypted.setCharAt(i, Character.toLowerCase(newChar));
-                }
+                char newChar = (i % 2 == 0) ? shiftedAlphabetOne.charAt(idx) : shiftedAlphabetTwo.charAt(idx);
+                setNewCharacter(currChar, newChar, encryptedMsg, i );
             }
         }
-        return encrypted.toString();
+        return encryptedMsg.toString();
+    }
+
+    private void setNewCharacter(char currChar, char newChar, StringBuilder encryptedMsg, int i ) {
+       char ch = Character.isUpperCase(currChar) ? Character.toUpperCase(newChar) : Character.toLowerCase(newChar);
+       encryptedMsg.setCharAt(i, ch);
     }
 
     private int validate(int key) {
@@ -68,6 +51,10 @@ public class CaesarCipher {
             return( key < 26 ) ? key : (key % 26);
         }
         throw new RuntimeException("This is not a valid encryption key: " + key);
+    }
+
+    private String shiftBy(int key) {
+        return ALPHABET.substring(key) + ALPHABET.substring(0, key);
     }
 
     public static void main(String[] args) {
